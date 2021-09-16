d69d2e8e1e8438a067aa651442465135d061037c
OmkarSaMo <omkmoh@biosustain.dtu.dk>
2021-09-16 12:07:18 +0000

    remove hidden folders

--- .snakemake/conda/66a0911470347801c428959a60e2d219/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/eeko/Desktop/Metabolomics/Snakemake/.snakemake/conda/66a0911470347801c428959a60e2d219/lib"
-XML2_LIBS="-lxml2 -L/Users/eeko/Desktop/Metabolomics/Snakemake/.snakemake/conda/66a0911470347801c428959a60e2d219/lib -lz -L/Users/eeko/Desktop/Metabolomics/Snakemake/.snakemake/conda/66a0911470347801c428959a60e2d219/lib -llzma -lpthread -L/Users/eeko/Desktop/Metabolomics/Snakemake/.snakemake/conda/66a0911470347801c428959a60e2d219/lib -liconv -L/Users/eeko/Desktop/Metabolomics/Snakemake/.snakemake/conda/66a0911470347801c428959a60e2d219/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/Users/eeko/Desktop/Metabolomics/Snakemake/.snakemake/conda/66a0911470347801c428959a60e2d219/include/libxml2 -I/Users/eeko/Desktop/Metabolomics/Snakemake/.snakemake/conda/66a0911470347801c428959a60e2d219/include"
-MODULE_VERSION="xml2-2.9.12"
-
