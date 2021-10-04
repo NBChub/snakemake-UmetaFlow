4a2834cf1e0299831289c78b397cf31e4af04fbb
eeko-kon <eeko@biosustain.dtu.dk>
2021-10-04 22:35:29 +0200

    OpenMS_workflow.ipynb examples of WF

--- workflow/scripts/OpenMSWF.py
+++ workflow/scripts/OpenMSWF.py
@@ -1,32 +1,17 @@
-"""
-Convert files from Thermo
-
-For Mac or Linux users (also windows):
-
-Download ThermoRawFileParser https://github.com/compomics/ThermoRawFileParser
-Linux: download mono https://www.mono-project.com/download/stable/#download-lin
-
-Run the command:
-
-mono ThermoRawFileParser.exe -d=/Users/eeko/Desktop/py4e/rawdata/mzML/ 
-
-this command will automatically convert the files to centroid mzML
-"""
-#import numpy as np 
-#import pandas as pd
 from pyopenms import *
-input_mzML = "./rawdata/mzML/CollinusKirromycin.mzML"
+from pandas import DataFrame
+import pandas as pd
+import pyteomics
+from pyteomics.openms import featurexml
+import numpy as np
+import sys
+from pyteomics import mztab
 
+example_filename= "Epemicins.mzML"
 exp = MSExperiment()
-print("Loading")
-MzMLFile().load(input_mzML, exp)
-print("Loaded")
-print(exp.getSourceFiles()[0].getNativeIDTypeAccession())
-print(exp.getSourceFiles()[0].getNativeIDType())
-
-# sorty my m/z
+MzMLFile().load(example_filename, exp)
 exp.sortSpectra(True)
-
+    
 delta_mzs= []
 mzs = []
 rts= []
@@ -36,25 +21,21 @@ PrecursorCorrection.correctToHighestIntensityMS1Peak(exp, 100.0, True, delta_mzs
 mass_traces = []
 mtd = MassTraceDetection()
 mtd_par = mtd.getDefaults()
-# set addition parameters values
-mtd_par.setValue("mass_error_ppm", 10.0) # example set ppm error
+mtd_par.setValue("mass_error_ppm", 10.0) 
 mtd_par.setValue("noise_threshold_int", 1.0e04)
 mtd.setParameters(mtd_par)
-print(mtd_par.getValue("mass_error_ppm")) # example check a specific value
-mtd.run(exp, mass_traces, 0)  # 0 is default and does not restrict found mass traces
-
+mtd.run(exp, mass_traces, 0)
 # Run elution peak detection
 mass_traces_split = []
 mass_traces_final = []
 epd = ElutionPeakDetection()
 epd_par = epd.getDefaults()
-# set additional parameter values
 epd_par.setValue("width_filtering", "fixed")
 epd.setParameters(epd_par)
 epd.detectPeaks(mass_traces, mass_traces_split)
-
+    
 if (epd.getParameters().getValue("width_filtering") == "auto"):
-    epd.filterByPeakWidth(mass_traces_split, mass_traces_final)
+     epd.filterByPeakWidth(mass_traces_split, mass_traces_final)
 else:
     mass_traces_final = mass_traces_split
 
@@ -63,49 +44,71 @@ feature_map_FFM = FeatureMap()
 feat_chrom = []
 ffm = FeatureFindingMetabo()
 ffm_par = ffm.getDefaults() 
-# set additional parameter values
 ffm_par.setValue("isotope_filtering_model", "none")
-ffm_par.setValue("remove_single_traces", "false")
-ffm_par.setValue("mz_scoring_by_elements", "false")
+ffm_par.setValue("remove_single_traces", "true")
+ffm_par.setValue("mz_scoring_by_elements", "true")
 ffm.setParameters(ffm_par)
 ffm.run(mass_traces_final, feature_map_FFM, feat_chrom)
-
-print('# Mass traces filtered:', len(mass_traces_final))
-
 feature_map_FFM.setUniqueIds()
 fh = FeatureXMLFile()
-print("Found", feature_map_FFM.size(), "features")
-fh.store('./pyOpenMS_results/FeatureFindingMetaboKirr.featureXML', feature_map_FFM)
+fh.store('./EpemicinsFeatureFindingMetabo.featureXML', feature_map_FFM)
 
 # Run metabolite adduct decharging detection
 # With SIRIUS you are only able to use singly charged adducts
 mfd = MetaboliteFeatureDeconvolution()
 mdf_par = mfd.getDefaults()
-# set additional parameter values
 mdf_par.setValue("potential_adducts",  [b"H:+:0.6",b"Na:+:0.2",b"NH4:+:0.1", b"H2O:-:0.1"])
 mdf_par.setValue("charge_min", 1, "Minimal possible charge")
 mdf_par.setValue("charge_max", 1, "Maximal possible charge")
 mdf_par.setValue("charge_span_max", 1)
 mdf_par.setValue("max_neutrals", 1)
-print(mdf_par.getValue("potential_adducts")) # test if adducts have been set correctly
 mfd.setParameters(mdf_par)
-
+    
 feature_map_DEC = FeatureMap()
 cons_map0 = ConsensusMap()
 cons_map1 = ConsensusMap()
 mfd.compute(feature_map_FFM, feature_map_DEC, cons_map0, cons_map1)
-fmdec= FeatureXMLFile()
-fmdec.store("./pyOpenMS_results/deconvolutedKirr.featureXML", feature_map_DEC)
+fxml = FeatureXMLFile()
+fh.store('./EpemicinsMFD.featureXML', feature_map_FFM)
+
+with featurexml.read('./EpemicinsMFD.featureXML') as f:
+    features_list = [FXML for FXML in f]
+    
+df = pd.DataFrame() 
+for feat in features_list:
+    idx = feat['id']
+    for key in feat.keys():
+        if key == 'id':
+           pass
+        elif key == 'position':
+            pos_list = feat['position']
+            for pos in pos_list:
+                if pos['dim'] == '0':
+                    df.loc[idx, 'position_0'] = pos['position']
+                elif pos['dim'] == '1':
+                    df.loc[idx, 'position_1'] = pos['position']
+        elif key == 'quality':
+            qual_list = feat['quality']
+            for qual in qual_list:
+                if qual['dim'] == '0':
+                    df.loc[idx, 'quality_0'] = qual['quality']
+                elif qual['dim'] == '1':
+                    df.loc[idx, 'quality_1'] = qual['quality']
+        else:
+            df.loc[idx, key] = feat[key]
+df_tidy = df.rename(columns = {'position_0': 'RT', 'position_1': 'mz'}, inplace = False)
+df_tidy=df_tidy.drop(columns= ["quality_0", "quality_1", "overallquality", "label", "legal_isotope_pattern"])
+df_tidy.reset_index(drop=True, inplace=True) 
+df_tidy.to_csv("EpemicinsFFM.csv")
 
 # Precursor corrector
 
 PrecursorCorrection.correctToNearestFeature(feature_map_DEC, exp, 0.0, 100.0, True, False, False, False, 3, 0)
 
-# Prepare sirius parameters
+# SIRIUS & CSI_FingerID
+
 sirius_algo = SiriusAdapterAlgorithm()
-
 sirius_algo_par = sirius_algo.getDefaults()
-
 sirius_algo_par.setValue("preprocessing:filter_by_num_masstraces", 2) 
 sirius_algo_par.setValue("preprocessing:precursor_mz_tolerance", 10.0) #default
 sirius_algo_par.setValue("preprocessing:precursor_mz_tolerance_unit", "ppm")
@@ -119,82 +122,50 @@ sirius_algo_par.setValue("sirius:elements_enforced", "CHNOS")
 sirius_algo_par.setValue("project:processors", 2)
 sirius_algo_par.setValue("fingerid:db", "BIO")
 sirius_algo.setParameters(sirius_algo_par)
-
-featureinfo = "./pyOpenMS_results/deconvolutedKirr.featureXML"
+    
+featureinfo = "./EpemicinsMFD.featureXML"
 fm_info = FeatureMapping_FeatureMappingInfo()
 feature_mapping = FeatureMapping_FeatureToMs2Indices() 
 sirius_algo.preprocessingSirius(featureinfo,
                                 exp,
                                 fm_info,
                                 feature_mapping)
-
-print("preprocessed")
-# Check feature and/or spectra number
-# https://github.com/OpenMS/OpenMS/blob/develop/src/utils/SiriusAdapter.cpp#L201
 sirius_algo.logFeatureSpectraNumber(featureinfo, 
                                     feature_mapping,
                                     exp)
-
-print("checked")
-
-# construct sirius ms file object
 msfile = SiriusMSFile()
-# create temporary filesystem objects
 debug_level = 3
 sirius_tmp = SiriusTemporaryFileSystemObjects(debug_level)
 siriusstring= String(sirius_tmp.getTmpMsFile())
-
-# fill variables, which are used in the function
-# this is a parameter, which is called "feature_only"
-# It is a boolean value (true/false) and if it is true you are using the  the feature information
-# from in_featureinfo to reduce the search space to MS2 associated with a feature.
-# this is recommended when working with featureXML input, if you do NOT use it
-# sirius will use every individual MS2 spectrum for estimation (and it will take ages)
-
 feature_only = sirius_algo.isFeatureOnly()
 isotope_pattern_iterations = sirius_algo.getIsotopePatternIterations()
 no_mt_info = sirius_algo.isNoMasstraceInfoIsotopePattern()
 compound_info = []
-
-msfile.store(exp, 
+msfile.store(exp,
              String(sirius_tmp.getTmpMsFile()),
              feature_mapping, 
              feature_only,
              isotope_pattern_iterations, 
              no_mt_info, 
              compound_info)
-print(String(sirius_tmp.getTmpMsFile()))
-print("stored")
-
-
-#next step:call siriusQprocess
-out_csifingerid = "./pyOpenMS_results/csifingerIDKirr.mzTab" # empty string, since no file was specified - no CSIFingerId Output will be generated
+out_csifingerid = "./Epemicins_csifingerID.mzTab" 
 executable= "/Users/eeko/Desktop/software/Contents/MacOS/sirius"
 subdirs = sirius_algo.callSiriusQProcess(String(sirius_tmp.getTmpMsFile()),
                                          String(sirius_tmp.getTmpOutDir()),
                                          String(executable),
                                          String(out_csifingerid),
-                                         False) # debug option not yet implemented
-print("SIRIUSQprocess")
-#SiriusMZtabwriter for storage
+                                         False)
 candidates = sirius_algo.getNumberOfSiriusCandidates()
 sirius_result = MzTab()
 siriusfile = MzTabFile()
 SiriusMzTabWriter.read(subdirs,
-                        input_mzML,
-                        candidates,
-                        sirius_result)
-print("storing..")
-siriusfile.store("./pyOpenMS_results/out_sirius_testKirr.mzTab", sirius_result)
-print("stored")
-
-#CSI:FingerID
-top_hits= 5
-csi_result=MzTab()
-csi_file=MzTabFile()
-CsiFingerIdMzTabWriter.read(subdirs,
-                    input_mzML,
-                    top_hits,
-                    csi_result)
-
-csi_file.store("./pyOpenMS_results/csifingerIDKirr.mzTab", csi_result)
+                       filename,
+                       candidates,
+                       sirius_result)
+siriusfile.store("./Epemicins_sirius.mzTab", sirius_result) 
+siriusfile= "./Epemicins_sirius.mzTab"
+sirius=  pyteomics.mztab.MzTab(siriusfile, encoding='UTF8', table_format='df')
+sirius.metadata
+df= sirius.small_molecule_table
+SIRIUS_DF= df.drop(columns= ["identifier", "smiles", "inchi_key", "description", "calc_mass_to_charge", "charge", "taxid", "species","database", "database_version", "spectra_ref", "search_engine", "modifications"])
+SIRIUS_DF.to_csv("./Epemicins_SIRIUS_DF.csv")
