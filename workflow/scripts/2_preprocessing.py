2d2085ce64c7ba83b7fda3283260ed2b51a11717
eeko-kon <eeko@biosustain.dtu.dk>
2021-10-11 14:58:48 +0200

    params failed_back to original

--- workflow/scripts/2_preprocessing.py
+++ workflow/scripts/2_preprocessing.py
@@ -2,7 +2,7 @@
 # 1) Mass trace detection: This algorithm gathers peaks similar in m/z and moving along retention time (filters according to intensity, frequency of gathered peaks in an apex and number of scans).
 # 2) Elution peak detection: This algorithm extracts chromatographic peaks from a mass trace. It first applies smoothing on the mass trace's intensities, then detects local minima/maxima in order to separate the chromatographic peaks from each other. The output of the algorithm is a set of chromatographic peaks for each mass trace (deconvolution).
 # 3) Feature detection: FeatureFinderMetabo assembles metabolite features from singleton mass traces. FeatureFinderMetabo fulfills a further data reduction step by assembling compatible mass traces to metabolite features (that is, all mass traces originating from one metabolite). To this end, multiple metabolite hypotheses are formulated and scored according to how well differences in RT (optional), m/z or intensity ratios match to those of theoretical isotope patterns.
-# 4) Metabolite adduct decharger: For each peak, this algorithm reconstructs neutral masses by enumerating all possible adducts with matching charge. You can add the list of adduct files and database files for the algorithm to parse through. With SIRIUS, an algorithm that is later used, you are only able to use singly charged adducts so charges higher than 1 are filtered out.
+# 4) Metabolite adduct dechargerc: For each peak, this algorithm reconstructs neutral masses by enumerating all possible adducts with matching charge. You can add the list of adduct files and database files for the algorithm to parse through. With SIRIUS, an algorithm that is later used, you are only able to use singly charged adducts so charges higher than 1 are filtered out.
 
 from pyopenms import *
 
@@ -24,8 +24,12 @@ def preprocessing(filename):
     epd_par.setValue("width_filtering", "fixed")
     epd.setParameters(epd_par)
     epd.detectPeaks(mass_traces, mass_traces_split)
-    epd.filterByPeakWidth(mass_traces_split, mass_traces_final)
 
+    if (epd.getParameters().getValue("width_filtering") == "auto"):
+        epd.filterByPeakWidth(mass_traces_split, mass_traces_final)
+    else:
+        mass_traces_final = mass_traces_split
+        
     feature_map_FFM = FeatureMap()
     feat_chrom = []
     ffm = FeatureFindingMetabo()
@@ -33,7 +37,6 @@ def preprocessing(filename):
     ffm_par.setValue("isotope_filtering_model", "none")
     ffm_par.setValue("remove_single_traces", "false")
     ffm_par.setValue("mz_scoring_by_elements", "true")
-    ffm_par.setValue("report_convex_hulls", "true")
     ffm.setParameters(ffm_par)
     ffm.run(mass_traces_final, feature_map_FFM, feat_chrom)
     feature_map_FFM.setUniqueIds()
