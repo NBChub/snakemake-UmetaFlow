5199562fc85045bfd7dbee842fcd837936f37b28
eeko-kon <eeko@biosustain.dtu.dk>
2021-10-24 20:01:27 +0200

    faster dataframe conversion

--- workflow/scripts/3_df_preprocess.py
+++ workflow/scripts/3_df_preprocess.py
@@ -7,52 +7,70 @@
 #isotope_distances = distance in mz between the isotopes (jumps of app. 1 is important to confirm that this is a real feature)
 
 from pyopenms import *
+
+from collections import defaultdict
+from functools import reduce
+from pathlib import Path
+from time import perf_counter
+import sys
+
+from IPython.core.display import display
+from pandas import CategoricalDtype
+import numpy as np
+from pyopenms import *
 import pandas as pd
-from pyteomics.openms import featurexml
+import os
 
+class FeatureMapDF(FeatureMap):      
+    def __init__(self):
+        super().__init__()
 
-with featurexml.read(snakemake.input[0]) as f:
-    features_list = [FXML for FXML in f]
-    
-df = pd.DataFrame() 
-for feat in features_list:
-    idx = feat['id']
-    for key in feat.keys():
-        if key == 'id':
-           pass
-        elif key == 'position':
-            pos_list = feat['position']
-            for pos in pos_list:
-                if pos['dim'] == '0':
-                    df.loc[idx, 'RT'] = pos['position']
-                elif pos['dim'] == '1':
-                    df.loc[idx, 'mz'] = pos['position']
-        elif key == 'intensity':
-            df.loc[idx, 'intensity'] = feat['intensity']
-        elif key == 'charge':
-            df.loc[idx, 'charge'] = feat['charge']
-        elif key == 'FWHM':
-            df.loc[idx, 'FWHM'] = feat['FWHM']
-        elif key == 'max_height':
-            df.loc[idx, 'max_height'] = feat['max_height']
-        elif key == 'num_of_masstraces':
-            df.loc[idx, 'num_of_masstraces'] = feat['num_of_masstraces']
-        elif key == 'masstrace_intesity':
-            df.loc[idx, 'masstrace_intesity'] = feat['masstrace_intesity']
-        elif key == 'masstrace_centroid_rt':
-            df.loc[idx, 'masstrace_centroid_rt'] = feat['masstrace_centroid_rt']
-        elif key == 'masstrace_centroid_mz':
-            df.loc[idx, 'masstrace_centroid_mz'] = feat['masstrace_centroid_mz']
-        elif key == 'isotope_distances':
-            df.loc[idx, 'isotope_distances'] = feat['isotope_distances']
-        elif key == "adducts":
-            if 'adducts' not in df.columns:
-                df['adducts'] = ''
-                df.loc[idx, "adducts"] = feat["adducts"]
+    def get_df(self):
+        def gen(fmap: FeatureMap, fun):
+            for f in fmap:
+                yield from fun(f)
+
+        def extractMetaData(f: Feature):
+            metavals = []
+            types = []
+            for k in metavals:
+                if k == b"num_of_masstraces":
+                    types.append('f')
+                elif k== b"FWHM":
+                    types.append('f')
+                elif k== b"max_height":
+                    types.append('i4')
+                elif k== b"masstrace_intensity":
+                    types.append("f")
+            # subfeatures = f.getFeatureList()  # type: list[FeatureHandle]
+            pep = f.getPeptideIdentifications()  # type: list[PeptideIdentification]
+            bb = f.getConvexHull().getBoundingBox2D()
+            for u in metavals:
+                f.getMetaValue(u)
+            if len(pep) != 0:
+                hits = pep[0].getHits()
+                if len(hits) != 0:
+                    besthit = hits[0]  # type: PeptideHit
+                    yield f.getUniqueId(), besthit.getSequence().toString(), f.getCharge(), f.getRT(), f.getMZ(), bb[0][0], bb[1][0], f.getMetaValue("num_of_masstraces"), f.getOverallQuality(), f.getIntensity()
+                else:
+                    yield f.getUniqueId(), None, f.getCharge(), f.getRT(), f.getMZ(), bb[0][0], bb[1][0], f.getMetaValue("num_of_masstraces"), f.getOverallQuality(), f.getIntensity()
             else:
-                df.loc[idx, "adducts"] = feat["adducts"]
-df= df[df["num_of_masstraces"]>=2.0]
-df=df.reset_index()
-df= df.rename(columns={"index": "FeatureID"})
-df= df.set_index("FeatureID")
-df.to_csv(snakemake.output[0])
+                yield f.getUniqueId(), None, f.getCharge(), f.getRT(), f.getMZ(), bb[0][0], bb[1][0], f.getMetaValue("num_of_masstraces"), f.getOverallQuality(), f.getIntensity()
+
+        cnt = self.size()
+
+        mddtypes = [('id', np.dtype('uint64')), ('sequence', 'U200'), ('charge', 'i4'), ('RT', 'f'), ('mz', 'f'),
+                    ('RTstart', 'f'), ('RTend', 'f'), ("num_of_masstraces", 'f'),
+                    ('quality', 'f'), ('intensity', 'f')]
+        mdarr = np.fromiter(iter=gen(self, extractMetaData), dtype=mddtypes, count=cnt)
+        df= pd.DataFrame(mdarr).set_index('id').sort_values("mz").drop(columns= "sequence")
+        df= df[df["num_of_masstraces"]>=2]
+        return df
+
+def preprocessDF(filename):
+    fmap = FeatureMapDF()
+    FeatureXMLFile().load(filename, fmap)
+    DF= fmap.get_df()
+    DF.to_csv(snakemake.output[0])
+
+filename=snakemake.input[0]
