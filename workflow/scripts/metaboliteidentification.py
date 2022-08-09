016ffdfbd997b4a818065b2b09e294679d404d83
eeko-kon <eeko@biosustain.dtu.dk>
2022-08-09 11:09:06 +0200

    guidance for -ESI

--- workflow/scripts/metaboliteidentification.py
+++ workflow/scripts/metaboliteidentification.py
@@ -34,6 +34,7 @@ def build_library(matrix, lib):
     DF_features["Charge"]= DF_features["Charge"].astype(str)
     DF_features["Mass"]= DF_features["Mass"].astype(float)
 
+#For positive ionisation: comment this for negative ESI
     for ind in DF_features.index:
         if DF_features["Charge"][ind] == "0":
             DF_features.loc[ind, "Mass"]= DF_features.loc[ind,"Mass"]- 1.007825
@@ -43,7 +44,6 @@ def build_library(matrix, lib):
             DF_features.loc[ind, "Mass"]= (DF_features.loc[ind,"Mass"]*2)- 2.015650
         if DF_features["Charge"][ind] == "3":
             DF_features.loc[ind, "Mass"]= (DF_features.loc[ind,"Mass"]*3)- 3.023475
-
     DF_features["Charge"]= DF_features["Charge"].astype(str)
     for ind in DF_features.index:
         if DF_features["Charge"][ind] == "0":
@@ -54,6 +54,27 @@ def build_library(matrix, lib):
             DF_features.loc[ind, "Charge"]= "+" + DF_features.loc[ind,"Charge"]
         if DF_features["Charge"][ind] == "3":
             DF_features.loc[ind, "Charge"]= "+" + DF_features.loc[ind,"Charge"]
+
+# For negative ionisation: uncomment this for negative ESI
+    # for ind in DF_features.index:
+    #     if DF_features["Charge"][ind] == "0":
+    #         DF_features.loc[ind, "Mass"]= DF_features.loc[ind,"Mass"]+ 1.007825
+    #     if DF_features["Charge"][ind] == "1":
+    #         DF_features.loc[ind, "Mass"]= DF_features.loc[ind,"Mass"]+ 1.007825
+    #     if DF_features["Charge"][ind] == "2":
+    #         DF_features.loc[ind, "Mass"]= (DF_features.loc[ind,"Mass"]*2)+ 2.015650
+    #     if DF_features["Charge"][ind] == "3":
+    #         DF_features.loc[ind, "Mass"]= (DF_features.loc[ind,"Mass"]*3)+ 3.023475
+    # DF_features["Charge"]= DF_features["Charge"].astype(str)
+    # for ind in DF_features.index:
+    #     if DF_features["Charge"][ind] == "0":
+    #         DF_features.loc[ind, "Charge"]= "-1"
+    #     if DF_features["Charge"][ind] == "1":
+    #         DF_features.loc[ind, "Charge"]= "-" + DF_features.loc[ind,"Charge"]
+    #     if DF_features["Charge"][ind] == "2":
+    #         DF_features.loc[ind, "Charge"]= "-" + DF_features.loc[ind,"Charge"]
+    #     if DF_features["Charge"][ind] == "3":
+    #         DF_features.loc[ind, "Charge"]= "-" + DF_features.loc[ind,"Charge"]
 
     DF_features["CompoundName"] = np.arange(len(DF_features))
     DF_features["CompoundName"] = "feature_" + DF_features["CompoundName"].astype(str)
